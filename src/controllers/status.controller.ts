a31cb0ee79a9a1c784f2b5f4e9baea5c6ff11e03
Mike Pattyn <mike@givtapp.net>
2022-05-31 16:43:59 +0200

    Added logging for the example

--- src/controllers/status.controller.ts
+++ src/controllers/status.controller.ts
@@ -10,8 +10,10 @@ export class StatusController extends BaseHttpController implements interfaces.C
     constructor(@inject(Logger) private logger: Logger) { 
         super() 
     }
+
     @httpGet('/')
     private health(req: Request, resp: Response) {
+        this.logger.log(`Received a ${req.method} request on: ${keyof({StatusController})}`)
         return new OkResult()
     }
 }
